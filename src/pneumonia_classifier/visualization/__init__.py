9c7038c74a77609f599c96c9a51d6af465dd5ca2
Carolina <carolreis0116@gmail.com>
2026-05-14 11:15:04 +0100

    viz: add plotting utilities for dataset and results

---
+++ src/pneumonia_classifier/visualization/__init__.py
@@ -0,0 +1,13 @@
+from pneumonia_classifier.visualization.plots import (
+    plot_class_distribution,
+    plot_confusion_matrix,
+    plot_metric_comparison,
+    plot_training_curves,
+)
+
+__all__ = [
+    "plot_class_distribution",
+    "plot_confusion_matrix",
+    "plot_metric_comparison",
+    "plot_training_curves",
+]
